2db0448d85fd31cea14188a50bf2a46b83326418
Alok Kuri <alokkuri98@gmail.com>
2026-06-02 20:45:53 +0600

    AddJobviews

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import './assets/main.css'
 import 'primeicons/primeicons.css'
+import 'vue-toastification/dist/index.css'
 import router from './router'
+import Toast from 'vue-toastification'
 
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+const options = {
+  position: 'top-right',
+  timeout: 3000
+}
 const app = createApp(App)
 app.use(router)
+app.use(Toast, options)
 app.mount('#app')
