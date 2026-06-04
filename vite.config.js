2d1922361b6515bca5158ed0de5024e0bdcc0f7b
Alok Kuri <alokkuri98@gmail.com>
2026-06-04 13:40:12 +0600

    OK

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       '/api':{
-        target: 'https://vue-json-backend-server.onrender.com/jobs',
+        target: 'https://vue-json-backend-server-1.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/,'')
       }
